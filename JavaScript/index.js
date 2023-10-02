0fe93374844cbaf12b63c400d3d79e4c00ccae29
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2023-10-02 20:37:48 +0200

    ajoue de filtre

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,14 +3,52 @@ const champs = await reponse.json();
 
 const champsZone = document.querySelector(".champsZone")
 
-console.log(champsZone);
 
-for(let i = 0; i< champs.length; i++){
-    const champCard = document.createElement("a")
-    const champName = champs[i].name
-    const ChampImgSrc = champs[i].icon
-    champCard.href = `./pagesChampions/${champName}`
-    champCard.innerHTML = `<h2>${champName}</h2><img src="${ChampImgSrc}">`
-    champsZone.appendChild(champCard)
-    console.log(champCard);
+const fonction = function(x){
+    for(let i = 0; i< champs.length; i++){
+        const champCard = document.createElement("a")
+        const champName = x[i].name
+        const ChampImgSrc = x[i].icon
+        champCard.href = `./pagesChampions/${champName}`
+        champCard.innerHTML = `<h2>${champName}</h2><img src="${ChampImgSrc}">`
+        champsZone.appendChild(champCard)
+    }
 }
+fonction(champs)
+
+
+// FILTRES // 
+
+const btnPdvDec = document.querySelector("#filtrePdvDec")
+const btnPdvCro = document.querySelector("#filtrePdvCro")
+
+btnPdvCro.addEventListener("click", () => {
+    const TriPdvCro = Array.from(champs)
+    TriPdvCro.sort(function(a, b){
+        return b.stats.hp - a.stats.hp
+    })
+    champsZone.innerHTML = ""
+    fonction(TriPdvCro)
+})
+
+btnPdvDec.addEventListener("click", () =>{
+    const TriPdvDec = Array.from(champs)
+    TriPdvDec.sort(function(a, b){
+        return a.stats.hp - b.stats.hp
+    })
+    champsZone.innerHTML = ""
+    fonction(TriPdvDec)
+
+
+})
+
+const FiltrePdvMax = document.querySelector("#trierPdvMax")
+
+
+FiltrePdvMax.addEventListener('input', () =>{
+    const ChampionsFiltres = champs.filter(function(x){
+        return x.stats.hp < FiltrePdvMax.value
+    })
+    champsZone.innerHTML =""
+    fonction(ChampionsFiltres);
+})
